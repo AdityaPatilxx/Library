169a314aeedf4d04acde8b70bee93b57f2d27265
AdityaPatilxx <161197993+AdityaPatilxx@users.noreply.github.com>
2025-01-31 16:50:46 +0000

    feat: add addBook button functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let myLibrary = []
 
-function Book(name, author, pages, hasRead) {
-    this.name = name
+function Book(title, author, pages, hasRead) {
+    this.title = title
     this.author = author
     this.pages = pages
     this.hasRead = hasRead
@@ -18,12 +18,14 @@ addBook.addEventListener('click', () => {
 
 })
 
+const booksContainer = document.querySelector('#booksContainer')
 const bookForm = document.querySelector('#bookForm')
 
 bookForm.addEventListener('submit', (event) => {
     event.preventDefault()
-    console.log(getBookFromInput())
-
+    const newBook = getBookFromInput()
+    makeBookCard(newBook)
+    dialog.close()
 })
 
 
@@ -32,5 +34,39 @@ function getBookFromInput() {
     const author = document.querySelector('#authorName').value
     const pages = document.querySelector('#bookLength').value
     const isRead = document.querySelector('#isRead').checked
-    return new Book(title,author,pages,isRead)
+    return new Book(title, author, pages, isRead)
+}
+
+function makeBookCard(book) {
+    const card = document.createElement('div')
+    const title = document.createElement('p')
+    const author = document.createElement('p')
+    const pages = document.createElement('p')
+    const buttonContainer = document.createElement('div')
+    const readButton = document.createElement('button')
+    const removeButton = document.createElement('button')
+
+
+    title.textContent = book.title
+    author.textContent = book.author
+    pages.textContent = `${book.pages} pages`
+
+    readButton.classList.add('btn')
+    removeButton.classList.add('btn')
+    removeButton.textContent = 'Remove'
+    buttonContainer.className = 'buttons'
+    card.className = 'card'
+
+    if (book.isRead == true) {
+        readButton.classList.add('btn-green')
+        readButton.textContent = 'Read'
+    }
+    else {
+        readButton.classList.add('btn-red')
+        readButton.textContent = 'Not Read'
+    }
+
+    buttonContainer.append(readButton, removeButton)
+    card.append(title, author, pages, buttonContainer)
+    booksContainer.append(card)
 }
