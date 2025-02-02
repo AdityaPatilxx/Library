cdad77d67f5aad1fb9c276dfb2e12f456dda4f51
AdityaPatilxx <161197993+AdityaPatilxx@users.noreply.github.com>
2025-02-02 14:49:27 +0000

    feat: implement library management functionality and update UI with footer

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-let myLibrary = []
+function Library() {
+    this.books = []
+
+    this.addBook = function (newBook) {
+        this.books.push(newBook)
+    }
+
+    this.removeBook = function (bookName) {
+        this.books = this.books.filter(currentBook => currentBook.title != bookName)
+    }
+
+    this.isInTheLibrary = function (bookName) {
+        return this.books.some(currentBook => currentBook.title == bookName)
+    }
+
+    this.getBook = function (bookName) {
+        return this.books.find((currentBook => currentBook.title == bookName))
+    }
+}
 
 function Book(title, author, pages, hasRead) {
     this.title = title
@@ -7,35 +25,52 @@ function Book(title, author, pages, hasRead) {
     this.hasRead = hasRead
 }
 
-// let harry = new Book('Harry Potter', 'J.K Rolling', '223', true)
-// makeBookCard(harry)
+let myLibrary = new Library()
+myLibrary.addBook(new Book('Harry Potter and the Sorcerers Stone', 'J.K. Rowling', 309, true))
+myLibrary.addBook(new Book('The Lord of the Rings', 'J.R.R. Tolkien', 1178, false))
+myLibrary.addBook(new Book('The Hunger Games', 'Suzanne Collins', 584, false))
+myLibrary.addBook(new Book('The Hobbit', 'J.R.R. Tolkien', 310, true))
 
-const addBook = document.querySelector('#addBook')
+
+const addBookButton = document.querySelector('#addBook')
 const dialog = document.querySelector('dialog')
+const booksContainer = document.querySelector('#booksContainer')
+const bookForm = document.querySelector('#bookForm')
 
-addBook.addEventListener('click', () => {
+addBookButton.addEventListener('click', () => {
     dialog.showModal()
 
 })
 
-const booksContainer = document.querySelector('#booksContainer')
-const bookForm = document.querySelector('#bookForm')
-
 bookForm.addEventListener('submit', (event) => {
     event.preventDefault()
     const newBook = getBookFromInput()
-    makeBookCard(newBook)
+    // makeBookCard(newBook)
+
+    myLibrary.addBook(newBook)
+    updateBookContainer()
+
     dialog.close()
 })
 
+function updateBookContainer() {
+    resetBookContainer()
+    myLibrary.books.forEach((book) => {
+        makeBookCard(book)
+    })
+}
+
+function resetBookContainer() {
+    booksContainer.innerHTML = ''
+}
 
 function getBookFromInput() {
     const title = document.querySelector('#bookName').value
     const author = document.querySelector('#authorName').value
     const pages = document.querySelector('#bookLength').value || 0
-    const isRead = document.querySelector('#isRead').checked
+    const hasRead = document.querySelector('#isRead').checked
     bookForm.reset()
-    return new Book(title, author, pages, isRead)
+    return new Book(title, author, pages, hasRead)
 }
 
 function makeBookCard(book) {
@@ -58,7 +93,7 @@ function makeBookCard(book) {
     buttonContainer.className = 'buttons'
     card.className = 'card'
 
-    if (book.isRead == true) {
+    if (book.hasRead == true) {
         readButton.classList.add('btn-green')
         readButton.textContent = 'Read'
     }
@@ -73,21 +108,30 @@ function makeBookCard(book) {
 }
 
 booksContainer.addEventListener('click', (event) => {
+
     const target = event.target
+
+    let BookName = target.parentElement.parentElement.firstElementChild.textContent
+    let book = myLibrary.getBook(BookName)
     if (target.textContent == 'Remove') {
+        myLibrary.removeBook(BookName)
         target.parentElement.parentElement.remove()
     }
 
     if (target.textContent == 'Read') {
+        book.hasRead = false
         target.textContent = 'Not Read'
         target.classList.remove('btn-green')
         target.classList.add('btn-red')
     }
     else if (target.textContent == 'Not Read') {
+        book.hasRead = true
         target.textContent = 'Read'
         target.classList.remove('btn-red')
         target.classList.add('btn-green')
     }
-
+    updateBookContainer()
 })
+    
+updateBookContainer()
 
